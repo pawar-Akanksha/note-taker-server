b0be9082129fb08e53727a7e90d16c407954dca8
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-25 02:39:20 -0700

    added fchanges

---
+++ Routes/delete.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const secret = "notetaker";
+const Note = require ("../Models/Note");
+const router = express.Router();
+router.use(express.json());
+router.use(cookieParser());
+
+
+router.post('/delete',async(req,res)=>{
+    const {token} = req.cookies;
+    jwt.verify(token,secret,{},async (err,info)=>{
+        if(err) throw err;
+        Note.deleteMany({id:info.id});
+    })
+})
